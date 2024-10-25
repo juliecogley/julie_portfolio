387684849eda7aeb516d0fa74a95019e7f534874
Rick Cogley <rick.cogley@esolia.co.jp>
2024-10-25 19:52:12 +0900

    Initial files for lume

---
+++ _config.ts
@@ -0,0 +1,40 @@
+import lume from "lume/mod.ts";
+import attributes from "lume/plugins/attributes.ts";
+import base_path from "lume/plugins/base_path.ts";
+import date from "lume/plugins/date.ts";
+import favicon from "lume/plugins/favicon.ts";
+import feed from "lume/plugins/feed.ts";
+import filter_pages from "lume/plugins/filter_pages.ts";
+import inline from "lume/plugins/inline.ts";
+import lightningcss from "lume/plugins/lightningcss.ts";
+import metas from "lume/plugins/metas.ts";
+import minify_html from "lume/plugins/minify_html.ts";
+import nav from "lume/plugins/nav.ts";
+import pagefind from "lume/plugins/pagefind.ts";
+import robots from "lume/plugins/robots.ts";
+import sitemap from "lume/plugins/sitemap.ts";
+import source_maps from "lume/plugins/source_maps.ts";
+import sri from "lume/plugins/sri.ts";
+import terser from "lume/plugins/terser.ts";
+
+const site = lume();
+
+site.use(attributes());
+site.use(base_path());
+site.use(date());
+site.use(favicon());
+site.use(feed());
+site.use(filter_pages());
+site.use(inline());
+site.use(lightningcss());
+site.use(metas());
+site.use(minify_html());
+site.use(nav());
+site.use(pagefind());
+site.use(robots());
+site.use(sitemap());
+site.use(source_maps());
+site.use(sri());
+site.use(terser());
+
+export default site;
