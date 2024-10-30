3845ea471a84097b409a8de6672d6435902c7452
Rick Cogley <rick.cogley@esolia.co.jp>
2024-10-30 10:46:48 +0900

    update cms config

--- _cms.ts
+++ _cms.ts
@@ -1,5 +1,115 @@
 import lumeCMS from "lume/cms/mod.ts";
+import GitHub from "lume/cms/storage/github.ts";
+import { Octokit } from "npm:octokit";
 
-const cms = lumeCMS();
+const username = Deno.env.get("USERNAME1")!;
+const password = Deno.env.get("PASSWORD1")!;
+
+const cms = lumeCMS({
+  site: {
+    name: "Julie Cogley Portfolio CMS",
+    description: "Edit the content of the Julie Cogley Portfolio site.",
+    url: "https://julie.cogley.jp",
+    body: `
+    <p>This is a monolingual portfolio site, with a top index page, an about page, showcase pages to provide narrative about projects, and as-is copies of specific sites.</p>
+    `,
+  },
+  auth: {
+    method: "basic",
+    users: {
+      // foo: "bar",
+      [username]: password,
+    },
+  },
+});
+
+// Register GitHub storage
+cms.storage(
+  "gh",
+  new GitHub({
+    client: new Octokit({ auth: Deno.env.get("GITHUB_TOKEN") }),
+    owner: "juliecogley",
+    repo: "julie_portfolio",
+    branch: "main",
+  }),
+);
+
+// Configure an upload folder
+cms.upload("assets", "gh:src/assets");
+
+// Pages collection
+cms.collection(
+  "showcase",
+  "gh:/src/showcase/*.vto",
+  [
+    {
+      name: "indextitle",
+      type: "text",
+      label: "Title for Top Index",
+      description: "Visible when hovering mouse over the top index image link",
+      attributes: {
+        required: true,
+      },
+    },
+    {
+      name: "metas.title",
+      type: "text",
+      label: "Title of the Page",
+      description: "Visible in browser tab and page source head section",
+      attributes: {
+        required: true,
+      },
+    },
+    {
+      name: "metas.description",
+      type: "textarea",
+      label: "Description for the Page",
+      description: "Visible in search engine results",
+      attributes: {
+        required: true,
+      },
+    },
+    {
+      name: "indeximage",
+      type: "file",
+      description: "Image to display on the top page.",
+      uploads: "assets",
+      attributes: {
+        accept: "image/*",
+        required: true,
+      },
+    },
+    {
+      name: "metas.image",
+      type: "file",
+      description: "Image to link in head metadata.",
+      uploads: "assets",
+      attributes: {
+        accept: "image/*",
+        required: true,
+      },
+    },    
+    {
+      name: "category",
+      type: "list",
+      label: "Category",
+      description: "Category to categorize the page on the top index",
+      init(field) {
+        field.options = ["clientwork", "work", "classwork", "hobby"];
+      },
+    },
+    "draft: checkbox",
+    {
+      name: "order",
+      type: "number",
+      label: "Order",
+      description: "Order in which the page will appear in the top menu. View all showcase pages at /showcase/ to see how the order number is currently set.",
+      attributes: {
+        required: true,
+      },
+    },
+    "content: markdown",
+  ],
+);
 
 export default cms;
