988c9817e7656122a961abfeac72f9c40cc1d078
James R. Cogley <rick.cogley@esolia.co.jp>
2024-11-09 20:35:34 +0900

    PR was merged, so reverting csp to main repo

--- serve.ts
+++ serve.ts
@@ -2,9 +2,9 @@ import Server from "lume/core/server.ts";
 import { basicAuth } from "lume/middlewares/basic_auth.ts";
 import precompress from "lume/middlewares/precompress.ts";
 import expires from "lume/middlewares/expires.ts";
-// import csp from "https://raw.githubusercontent.com/lumeland/experimental-plugins/main/csp/mod.ts";
+import csp from "https://raw.githubusercontent.com/lumeland/experimental-plugins/main/csp/mod.ts";
 
-import csp from "https://raw.githubusercontent.com/RickCogley/experimental-plugins/refs/heads/main/csp/mod.ts";
+//import csp from "https://raw.githubusercontent.com/RickCogley/experimental-plugins/refs/heads/main/csp/mod.ts";
 
 const server = new Server({
   port: 8000,
