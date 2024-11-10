f7969a21c3172f35913a354e1d1baee46fb2db77
James R. Cogley <rick.cogley@esolia.co.jp>
2024-11-10 20:51:02 +0900

    Fetch holidays and create const dates for vento use

--- src/_data.ts
+++ src/_data.ts
@@ -1,1 +1,19 @@
 export const cacheBuster = `${new Date().getTime()}`;
+export const todaysDateYYYYMMDD = `${new Date().toISOString().split("T")[0]}`;
+
+// export const todaysDateYYYYMMDD2 = `${new Date().toISOString().slice(0, 10)}`;
+
+export const todaysDateJAJP = `${new Date().toLocaleString('ja-JP',{ year: 'numeric', month: 'long', day: 'numeric', weekday: 'long' })}`;
+
+const response = await fetch('https://holidays-jp.github.io/api/v1/date.json', {
+  method: 'GET',
+  mode: 'no-cors',
+  headers: {
+    'Accept': 'application/json'
+  }
+});
+const holidays = await response.json();
+
+export {
+  holidays,
+};
