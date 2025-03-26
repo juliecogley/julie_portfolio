c0c7e0348adcf6b067448ceb8c8596faf4d1f545
Rick Cogley <rick.cogley@esolia.co.jp>
2025-03-26 08:26:07 +0000

    Add setup script to devcontainer

---
+++ setup.sh
@@ -0,0 +1,12 @@
+# setup.sh to be auto-run from devcontainer.json on codespace creation
+echo "= ADD PATHS and INITS to .bashrc ="
+echo 'export PATH="/home/node/.deno/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(starship init bash)"' >> ~/.bashrc
+
+echo "= INSTALL LUME CLI ="
+deno install --allow-run --allow-env --allow-read --name lume --force --reload --global https://deno.land/x/lume_cli/mod.ts
+
+echo "= INSTALL STARSHIP PROMPT ="
+curl -sS https://starship.rs/install.sh | sh -s -- -y && . ~/.bashrc
+
+echo "= PREP COMPLETE ="
